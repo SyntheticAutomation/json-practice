7ea2a4690be5ddbe0659900f7f4d3868f808290d
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-25 10:45:06 -0700

    Adds test suite and code

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,24 @@
-#code goes here
+require 'json'
+
+
+class Story
+
+  attr_reader :section, :subsection, :title, :abstract, :url, :published_date, :photo
+
+  def initialize(story_index, file_path)
+    file = File.read('data/nytimes.json')
+    data_hash = JSON.parse(file)
+    list_of_stories = data_hash["results"]
+
+    @section = list_of_stories[story_index]["section"]
+    @subsection = list_of_stories[story_index]["subsection"]
+    @title = list_of_stories[story_index]["title"]
+    @abstract = list_of_stories[story_index]["abstract"]
+    @url = list_of_stories[story_index]["url"]
+    @published_date = list_of_stories[story_index]["published_date"]
+    @photo = list_of_stories[story_index]["multimedia"][0]["url"]
+  end
+
+
+
+end
---
+++ test/story_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require 'json'
+require './lib/story'
+
+
+class StoryTest < Minitest::Test
+  def test_it_exists
+    story = Story.new(0, 'data/nytimes.json')
+    assert_instance_of Story, story
+  end
+
+  def test_it_has_attributes
+    story = Story.new(0, 'data/nytimes.json')
+    assert_equal "U.S.", story.section
+    assert_equal "Politics", story.subsection
+    assert_equal "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal", story.title
+    assert_equal "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.", story.abstract
+    assert_equal "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html", story.url
+    assert_equal "2018-11-29T18:46:27-05:00", story.published_date
+    assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-thumbStandard.jpg", story.photo
+  end
+end
